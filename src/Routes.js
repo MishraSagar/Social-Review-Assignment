57c9a999bd1027af4849579fa7666caa016dbb30
MishraSagar <sagar@newput.com>
2018-11-02 09:12:40 +0530

    moved App.js to components

---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+const routes = (
+    <Switch>
+        <Route path="/" component="{Timeline}" />
+        <Route path="/about" component="{About}" />
+        <Route path="/photos" component="{Photos}" />
+        <Route path="/friends" component="{Friends}" />
+    </Switch>
+);
+
+export routes;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './Components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './assets/styles/index.scss';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App>
+
+    </App>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
