5e3eb8b34ef25fd2fde010208d68ea0b5a6f1ce5
MishraSagar <sagar@newput.com>
2018-11-06 09:37:53 +0530

    Resolved issues with about content

--- src/Components/App.js
+++ src/Components/App.js
@@ -22,8 +22,16 @@ class App extends Component {
 
                         <div className="main-content">
                             <div className="row">
-                                <div className="profile-content col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                                <div className="profile-content col-xs-12 col-sm-4 col-md-3 col-lg-3">
                                     <Profile {...userinfo} userID={0}/>
+                                </div>
+
+                                <div className="about-container col-xs-12 col-sm-8 col-md-9 col-lg-9">
+                                    <div className="row">
+                                        <div className="about-content">
+                                        
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
