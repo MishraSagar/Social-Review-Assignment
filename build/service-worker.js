390dfc8171da90cd7e8ac903484083b2ec296ec9
MishraSagar <sagar@newput.com>
2018-12-05 09:39:28 +0530

    Modified chart routes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d874df0af25e4d65a7f681c24a4e6d9f.js"
+  "/precache-manifest.f788029dcaffb494865dc76b9c56906a.js"
 );
 
 workbox.clientsClaim();
--- src/Routes.js
+++ src/Routes.js
@@ -14,7 +14,7 @@ export default (props) => {
         <div>
             <Switch>
                 <Route exact path="/login" render={() => props.isUserLoggedIn ? <Redirect to="/dashboard" /> : <Login userLogin={props.userLogin} />} />
-                {/* <Route path="/charts" render={() => props.isUserLoggedIn ? <Redirect to="/charts/bar" /> : <Redirect to="/login" />} /> */}
+                <Route exact path="/charts" render={() => props.isUserLoggedIn ? <Redirect to="/charts/bar" /> : <Redirect to="/login" />} />
                 <Route path="/charts/:content" render={() => props.isUserLoggedIn ? <Charts /> : <Redirect to="/login" />} />
                 <Route path="/dashboard/:content" render={() => props.isUserLoggedIn ? <Dashboard usersData={props.usersData} userID={props.userID}/> : <Redirect to="/login" />} />
                 <Route path="/registration" render={() => props.isUserLoggedIn ? <Redirect to="/dashboard" /> : <Registration userLogin={props.userLogin}/>}/>
